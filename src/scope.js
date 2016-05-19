40916304c2954b70502c3041d4aa6928b01cacdb
Jacob Israel Turner <j.israel.turner@gmail.com>
2016-05-18 23:24:34 -0600

    Add basic scope watch, listen, and dirty checking

--- src/scope.js
+++ src/scope.js
@@ -1,1 +1,21 @@
-export default function() {}
+import { forEach } from 'lodash'
+
+export default class Scope {
+  constructor() {
+    this.$$watchers = []
+  }
+  $watch(watchFn, listenerFn) {
+    const watcher = { watchFn, listenerFn, last: Symbol() }
+    this.$$watchers.push(watcher)
+  }
+  $digest() {
+    forEach(this.$$watchers, watcher => {
+      const newVal = watcher.watchFn(this)
+      const oldVal = watcher.last
+      if (newVal !== oldVal) {
+        watcher.last = newVal
+        watcher.listenerFn(newVal, oldVal, this)
+      }
+    })
+  }
+}
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1,10 +1,62 @@
 import Scope from '../src/scope'
 
-describe('Scope', function () {
-  it("can be constructed and used as an object", function() {
+describe('Scope', () => {
+  it("can be constructed and used as an object", () => {
     var scope = new Scope()
     scope.propertyA = 1
-
     expect(scope.propertyA).toBe(1)
+  })
+
+  describe('digest', () => {
+    let scope
+
+    beforeEach(() =>scope = new Scope())
+
+    it('calls the listener function of a watch on first $digest', () => {
+      const watchFn = () => 'wat'
+      const listenerFn = jasmine.createSpy()
+      scope.$watch(watchFn, listenerFn)
+      scope.$digest()
+      expect(listenerFn).toHaveBeenCalled()
+    })
+
+    it('calls the watch function with the scope as the argument', () => {
+      var watchFn = jasmine.createSpy()
+      var listenerFn = () => 'nothing'
+      scope.$watch(watchFn, listenerFn)
+      scope.$digest()
+      expect(watchFn).toHaveBeenCalledWith(scope)
+    })
+
+    it('calls the listener function when the watched function changes', () => {
+      scope.someValue = 'a'
+      scope.counter = 0
+
+      scope.$watch((scope) => scope.someValue, (oldVal, newVal, scope) => scope.counter++)
+
+      expect(scope.counter).toBe(0)
+
+      scope.$digest()
+      expect(scope.counter).toBe(1)
+
+      scope.$digest()
+      expect(scope.counter).toBe(1)
+
+      scope.someValue = 'b'
+      expect(scope.counter).toBe(1)
+
+      scope.$digest()
+      expect(scope.counter).toBe(2)
+    })
+
+    it('calls listener when watch value is first undefined', () => {
+      scope.counter = 0
+
+      scope.$watch((scope) => scope.someValue, (newVal, oldVal, scope) => scope.counter++)
+
+      scope.$digest()
+      expect(scope.counter).toBe(1)
+    })
+
   })
 })
